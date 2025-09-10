9967c852f5911a31e20d1e3f20189da7d4efc470
Sanjyrana <Sanjayrana5113@gmail.com>
2025-09-10 13:34:34 +0530

    logout route changes

--- Server/controller/auth-controller.js
+++ Server/controller/auth-controller.js
@@ -210,7 +210,12 @@ const loginController = async (req, res) => {
 
 const logoutController = (req, res) => {
     try {
-        res.clearCookie("token")
+        res.clearCookie("token","",{
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            expires: new Date(0)
+        })
         res.status(200).json({
             message: "Logged out successfully",
             success: true
