ad6cf283114ae7d123337565af6aaabdfb935e6e
Sanjyrana <Sanjayrana5113@gmail.com>
2025-09-10 13:40:09 +0530

    logout route

--- Server/controller/auth-controller.js
+++ Server/controller/auth-controller.js
@@ -210,7 +210,7 @@ const loginController = async (req, res) => {
 
 const logoutController = (req, res) => {
     try {
-        res.Cookie("token","",{
+        res.cookie("token","",{
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
             sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
