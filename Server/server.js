c3c591d6fa5fe8124a3aec63d1283f30c8b51245
Sanjyrana <Sanjayrana5113@gmail.com>
2025-09-10 09:43:37 +0530

    changes in server.js

--- Server/server.js
+++ Server/server.js
@@ -12,16 +12,19 @@ const TempUserModel = require("./models/temperory-user-model")
 const app = express()
 app.use(cookieParser())
 app.use(cors({
-    origin:process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials:true
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true
 }))
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8000;
 // const __dirname = path.resolve()
 app.use(express.json())  // allwos to parse incoming requests in JSON body : req.body
 
+app.get("/", (req, res) => {
+    res.send("API is running 🚀")
+})
 //signup,login or logout routes
-app.use("/api/auth",authRouter)
+app.use("/api/auth", authRouter)
 // if(process.env.NODE_ENV === "production"){
 //     app.use(express.static(path.join(__dirname,"/Frontend/dist")))
 
@@ -32,13 +35,16 @@ app.use("/api/auth",authRouter)
 
 // DB connection
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is running at port : ${PORT}`); 
-
-
-        //delete temeperory users automatically
-        cron.schedule("*/5 * * * *",async () => {
-            await TempUserModel.deleteMany({verificationTokenExpiredAt : {$lt : Date.now()}})
+    if (process.env.NODE_ENV !== "production") {
+        app.listen(PORT, () => {
+            console.log(`Server is running at port : ${PORT}`);
         })
+    }
+
+
+
+    //delete temeperory users automatically
+    cron.schedule("*/5 * * * *", async () => {
+        await TempUserModel.deleteMany({ verificationTokenExpiredAt: { $lt: Date.now() } })
     })
 })
